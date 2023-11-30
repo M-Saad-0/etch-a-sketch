f976bcfdbc161c4068dfaff3d7a0f4baec9dccb1
M-Saad-0 <msad8961@gmail.com>
2023-11-30 22:54:28 +0500

    added a random color picker

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-createCanvas(64);
+createCanvas(45);
 slider();
 getColor();
+pickAcolor();
+getColorFromColorPicker();
+painting();
+
 function slider() {
     const sld = document.querySelector(".slider");
     const txt = document.querySelector(".size");
     sld.onchange = (e)=>{
         removeChildren();
         createCanvas(e.target.value);
-        txt.innerHTML = `${e.target.value}x${e.target.value}`
+        txt.innerHTML = `${e.target.value}x${e.target.value}`;
+        painting();
     };
 }
 
 function createCanvas(size) {
     const board = document.querySelector(".sec2");
     const blockNumber = 65 - size;
-    console.log(blockNumber);
-    console.log(board);
     board.style.gridTemplateColumns = `repeat(${blockNumber}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${blockNumber}, 1fr)`;
     for(let i=0; i<blockNumber*blockNumber; i++)
@@ -34,7 +37,7 @@ function removeChildren() {
     }
 }
 
-let color;
+let color = "black";
 function getColor() {
     const colors = document.querySelectorAll(".c");
     for(let i=0; i<colors.length; i++) {
@@ -47,14 +50,13 @@ function getColor() {
 const colors1 = document.querySelector(".colorPicker");
 const container = document.querySelector(".container");
 function pickAcolor() {
-    container.classList.add("blur");
-    colors1.style.display = "grid";
+    const colors1 = document.querySelector(".colorPicker");
     colors1.style.gridTemplateColumns = `repeat(15, 1fr)`;
     colors1.style.gridTemplateRows = `repeat(15, 1fr)`;
     for(let i=0; i<15*15; i++) {
         const colorToChoose = document.createElement("div");
         let rand = Math.floor(Math.random()*16777215);
-        color = `#${rand.toString(16)}`;
+        let color = `#${rand.toString(16)}`;
         colorToChoose.style.backgroundColor = color;
         colorToChoose.classList.add(color);
         colorToChoose.classList.add("p");
@@ -64,23 +66,31 @@ function pickAcolor() {
 
 function getColorFromColorPicker() {
     const colors = document.querySelectorAll(".p");
-    console.log(colors);
-    for(let i=0; i<15*15; i++) {
+    for(let i=0; i<colors.length; i++) {
         colors[i].addEventListener("click", (e)=>{
-            e.stopPropagation();
             color = e.target.classList[0];
+            container.classList.remove("blur");
+            colors1.style.display = "none";
         });
     }
-    container.classList.remove("blur");
-    colors1.style.display = "none"
+    
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", (e)=>{
-    pickAcolor();
-    getColorFromColorPicker();
+    colors1.style.display = "grid";
+    container.classList.add("blur");
 });
 
 function painting() {
-    
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach(b => {
+        b.addEventListener("mouseover", (e)=> {
+            e.stopPropagation();
+            if (color==="rainbow")
+            b.style.backgroundColor = `#${(Math.floor(Math.random()*16777215)).toString(16)}`;
+            else
+                b.style.backgroundColor = color;
+        });
+    });
 }
